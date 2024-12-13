7c8697384855f55b7be24093eeccc24c68c5678b
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-12-13 19:02:19 +0530

    chore: update CORS configuration and add web.config for Azure deployment

--- Server/app.js
+++ Server/app.js
@@ -18,7 +18,10 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "https://polite-dune-01a2ef300.4.azurestaticapps.net/",
+      "http://localhost:3000",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
